ae9bb7ee0183fcd794e30ee5d2be0a20852be816
Ram Gopal Reddy <redabothularamgopalreddy@gmail.com>
2025-04-20 01:23:44 +0530

    Update Styles

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,6 @@
 import { useAuth } from "./context/AuthContext";
+import { Button } from "./components/ui/button";
+import { Avatar,AvatarFallback,AvatarImage } from "@radix-ui/react-avatar";
 
 
 export const Home = () => {
@@ -8,21 +10,23 @@ export const Home = () => {
     const displayUser=user?.user_metadata.user_name || user?.email;
 
     return(
-        <div>
+        <div className="bg-black h-screen flex items-center justify-center">
 
             {user?(
-                <div>
-                    <img src={user?.user_metadata.avatar_url} alt="avatar" width={100} height={100}/>
+                <div className="bg-white p-10 rounded-lg flex flex-col items-center justify-center gap-4">
+                    <Avatar>
+                        <AvatarImage src={user?.user_metadata.avatar_url} className="rounded-full w-16 h-16 sm:w-24 sm:h-24 lg:w-32 lg:h-32"/>
+                    </Avatar>
                     <p>{user?.email}</p>
                     <h1>Welcome {displayUser}</h1>
-                    <button onClick={signOut}>
+                    <Button variant="destructive" onClick={signOut}>
                         Sign out
-                    </button>
+                    </Button>
                 </div>
             ):(
-            <button onClick={signInWithGitHub}>
+            <Button onClick={signInWithGitHub}>
                 Sign with GitHub
-            </button>
+            </Button>
             )}
             
         </div>
