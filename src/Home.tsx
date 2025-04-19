d9a74269c5a5f30402d6bf89a68339c41bcadc12
Ram Gopal Reddy <redabothularamgopalreddy@gmail.com>
2025-04-20 03:57:12 +0530

    Update

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,53 @@
+'use client';
+import { SlidingNumber } from "./components/motion-primitives/sliding-number";
+
 import { useAuth } from "./context/AuthContext";
 import { Button } from "./components/ui/button";
 import { Avatar,AvatarImage } from "@radix-ui/react-avatar";
+import { useEffect, useState } from "react";
 
 
 export const Home = () => {
+
+    const [hours, setHours] = useState(new Date().getHours());
+    const [minutes, setMinutes] = useState(new Date().getMinutes());
+    const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+        useEffect(() => {
+            const interval = setInterval(() => {
+                const now = new Date();
+                setHours(now.getHours());
+                setMinutes(now.getMinutes());
+                setSeconds(now.getSeconds());
+            }, 1000);
+
+            return () => clearInterval(interval);
+        }, []);
 
     const{signInWithGitHub,signOut,user}=useAuth()
 
     const displayUser=user?.user_metadata.user_name || user?.email;
 
     return(
+            
         <div className="bg-black h-screen flex items-center justify-center">
 
             {user?(
                 <div className="bg-white p-10 rounded-lg flex flex-col items-center justify-center gap-4">
                     <Avatar>
-                        <AvatarImage src={user?.user_metadata.avatar_url} className="rounded-full w-16 h-16 sm:w-24 sm:h-24 lg:w-32 lg:h-32"/>
+                        <AvatarImage src={user?.user_metadata.avatar_url} className="rounded-full w-18 h-18 sm:w-26 sm:h-26 lg:w-40 lg:h-40"/>
                     </Avatar>
                     <p>{user?.email}</p>
                     <h1>Welcome {displayUser}</h1>
                     <Button variant="destructive" onClick={signOut}>
                         SignOut
                     </Button>
+
+                    <div className="flex gap-2">
+                        <SlidingNumber value={hours} />
+                        <SlidingNumber value={minutes} />
+                        <SlidingNumber value={seconds} />
+                    </div>
                 </div>
             ):(
             <Button onClick={signInWithGitHub} className="bg-white text-black hover:bg-white-200">
